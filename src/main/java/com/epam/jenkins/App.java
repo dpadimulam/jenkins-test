6ee368a6d2f2afa9e721aa44c26834ae5a9f4cca
Durga Adimulam <Durga_Adimulam@epam.com>
2020-03-23 11:31:40 +0530

    update with if

--- src/main/java/com/epam/jenkins/App.java
+++ src/main/java/com/epam/jenkins/App.java
@@ -9,5 +9,39 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int count = 0;
+        if (count == 0) {
+        	System.out.println("Print 0");
+        } else if (count == 1) {
+        	System.out.println("Print 1");
+        } else if (count == 2) {
+        	System.out.println("Print 2");
+        } else if (count == 3) {
+        	System.out.println("Print 3");
+        } else if (count == 4) {
+        	System.out.println("Print 4");
+        } else if (count == 5) {
+        	System.out.println("Print 5");
+        } else if (count == 6) {
+        	System.out.println("Print 6");
+        } else if (count == 7) {
+        	System.out.println("Print 7");
+        } else if (count == 8) {
+        	System.out.println("Print 8");
+        } else if (count == 9) {
+        	System.out.println("Print 9");
+        } else if (count == 10) {
+        	System.out.println("Print 10");
+        } else if (count == 11) {
+        	System.out.println("Print 11");
+        } else if (count == 12) {
+        	System.out.println("Print 12");
+        } else if (count == 13) {
+        	System.out.println("Print 13");
+        } else if (count == 14) {
+        	System.out.println("Print 14");
+        } else if (count == 15) {
+        	System.out.println("Print 15");
+        }
     }
 }
